3d34411dfb1d13e834b81482353548f9f6d70d0e
Phong <phongvt2008@gmail.com>
2022-06-09 13:54:59 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+/*
 function App() {
   return (
     <div className="App">
@@ -21,5 +38,6 @@ function App() {
     </div>
   );
 }
+*/
 
 export default App;
