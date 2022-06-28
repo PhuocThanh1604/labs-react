1df77408da447443fa42ec061757c0fcadcbb78c
thanhvp <phuocthanh20cgh@gmail.com>
2022-06-28 16:26:56 +0700

    contactComponent

--- Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/ContactComponent.js
+++ Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/ContactComponent.js
@@ -32,7 +32,7 @@ function Contact(props) {
             <br />
             <i className="fa fa-phone"></i>: +852 1234 5678
             <br />
-            <i className="fa fa-fax"></i>: +852 8765 4321
+            <i className="fa fa-fax"></i>: +852 8764 4321
             <br />
             <i className="fa fa-envelope"></i>:{" "}
             <a href="mailto:confusion@food.net">confusion@food.net</a>
@@ -58,17 +58,17 @@ function Contact(props) {
               className="btn btn-primary"
               href="tel:+85212345678"
             >
-              <i className="fa fa-phone"></i> Call
+              <i className="fa fa-phone"></i>Call
             </a>
             <a role="button" className="btn btn-info">
-              <i className="fa fa-skype"></i> Skype
+              <i className="fa fa-skype"></i>Skype
             </a>
             <a
               role="button"
               className="btn btn-success"
               href="mailto:confusion@food.net"
             >
-              <i className="fa fa-envelope-o"></i> Email
+              <i className="fa fa-envelope-o"></i>Email
             </a>
           </div>
         </div>
