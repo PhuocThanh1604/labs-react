bf9b7f44693d7dfe4e6680eb7dd80e1f70757063
thanhvp <phuocthanh20cgh@gmail.com>
2022-06-30 16:52:52 +0700

    Intro. to Redux

--- Lab7-SE151066_VOPHUOCTHANH_SE151066/React/confusion/src/App.js
+++ Lab7-SE151066_VOPHUOCTHANH_SE151066/React/confusion/src/App.js
@@ -2,16 +2,29 @@ import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Main from './components/MainComponent';
 import './App.css';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore';
 
+
+const store = ConfigureStore();
 class App extends Component {
   
   render() {
     return (
-      <BrowserRouter>
-        <div className="App">
-          <Main />
-        </div>
-      </BrowserRouter>
+      // <BrowserRouter>
+      //   <div className="App">
+      //     <Main />
+      //   </div>
+      // </BrowserRouter>
+
+        <Provider store={store}>
+        <BrowserRouter>
+          <div className="App">
+            <Main />
+          </div>
+        </BrowserRouter>
+      </Provider>
+
     );
   }  
 
