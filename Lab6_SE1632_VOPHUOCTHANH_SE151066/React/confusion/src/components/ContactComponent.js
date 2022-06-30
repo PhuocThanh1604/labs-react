2969b07b3036c90e2c5b1876839106d5141427cf
thanhvp <phuocthanh20cgh@gmail.com>
2022-06-30 16:57:07 +0700

    Single Page Applications Part 2

--- Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/ContactComponent.js
+++ Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/ContactComponent.js
@@ -42,7 +42,7 @@ function Contact(props) {
           <h5>Map of our Location</h5>
           <iframe
             title="address"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.5236503473993!2d114.21604661542818!3d22.333848147318914!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340406b77660041b%3A0x1e1faa9bb38e36f3!2s121%20Clear%20Water%20Bay%20Rd%2C%20Ngau%20Chi%20Wan%2C%20Hong%20Kong!5e0!3m2!1sen!2s!4v1656124059314!5m2!1sen!2s"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5541.747757359264!2d106.80959397192429!3d10.841325463960219!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31752731176b07b1%3A0xb752b24b379bae5e!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBGUFQgVFAuIEhDTQ!5e0!3m2!1svi!2s!4v1656571316786!5m2!1svi!2s"
             width="600"
             height="450"
             allowfullscreen=""
