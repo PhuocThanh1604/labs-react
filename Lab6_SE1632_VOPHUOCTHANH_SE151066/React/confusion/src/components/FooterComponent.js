50287a870f2d849d8fab01ae3bd4e758faf7e88b
thanhvp <phuocthanh20cgh@gmail.com>
2022-07-13 23:45:26 +0700

    React Router

--- Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/FooterComponent.js
+++ Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/FooterComponent.js
@@ -22,7 +22,7 @@ function Footer(props) {
                 <div className="col-7 col-sm-5">
                     <h5>Our Address</h5>
                     <address>
-		              121, Clear Water Bay Road<br />
+		              121, Clear WaterBay Road<br />
 		              Clear Water Bay, Kowloon<br />
 		              HONG KONG<br />
 		              <i className="fa fa-phone fa-lg"></i>: +852 1234 5678<br />
