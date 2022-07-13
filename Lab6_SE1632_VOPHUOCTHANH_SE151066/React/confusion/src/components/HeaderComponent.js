b263270ce88fca25e9747a22621d7f803550c248
thanhvp <phuocthanh20cgh@gmail.com>
2022-07-13 23:40:37 +0700

    Header and Footer

--- Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/HeaderComponent.js
+++ Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/components/HeaderComponent.js
@@ -36,7 +36,7 @@ class Header extends Component {
                            </NavItem>
                            <NavItem>
                               <NavLink className="nav-link" to="/aboutus">
-                                 <span className="fa fa-info fa-lg"></span> About Us
+                                 <span className="fa fa-info fa-lg"></span> AboutUs
                               </NavLink>
                            </NavItem>
                            <NavItem>
