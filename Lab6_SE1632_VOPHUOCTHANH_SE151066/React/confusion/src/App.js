a72b52f41ad24d187f46e50e230b40d143cf4dee
thanhvp <phuocthanh20cgh@gmail.com>
2022-07-13 23:37:06 +0700

    Presentational and Container Components

--- Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/App.js
+++ Lab6_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import Main from './components/MainComponent';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter } from "react-router-dom";
+import Main from "./components/MainComponent";
+import "./App.css";
 
 class App extends Component {
-  
   render() {
     return (
       <BrowserRouter>
@@ -13,8 +12,7 @@ class App extends Component {
         </div>
       </BrowserRouter>
     );
-  }  
-
+  }
 }
 
 export default App;
