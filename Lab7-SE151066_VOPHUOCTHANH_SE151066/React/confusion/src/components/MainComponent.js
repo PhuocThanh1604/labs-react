85da7a196083d926eeed00b330b6c431c0aa9279
thanhvp <phuocthanh20cgh@gmail.com>
2022-07-13 23:23:13 +0700

    Intro. to Redux

--- Lab7-SE151066_VOPHUOCTHANH_SE151066/React/confusion/src/components/MainComponent.js
+++ Lab7-SE151066_VOPHUOCTHANH_SE151066/React/confusion/src/components/MainComponent.js
@@ -37,14 +37,13 @@ class Main extends Component {
   }
 
   onDishSelect(dishId) {
-    this.setState({ selectedDish: dishId });
+    this.setState({selectedDish: dishId });
   }
 
   render() {
     const HomePage = () => {
       return(
-          <Home 
-              dish={this.props.dishes.filter((dish) => dish.featured)[0]}
+          <Home dish={this.props.dishes.filter((dish) => dish.featured)[0]}
               promotion={this.props.promotions.filter((promo) => promo.featured)[0]}
               leader={this.props.leaders.filter((leader) => leader.featured)[0]}
           />
@@ -52,7 +51,7 @@ class Main extends Component {
     }
 
     const DishWithId = ({match}) => {
-      return(
+      return(  
           <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
             comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
       );
