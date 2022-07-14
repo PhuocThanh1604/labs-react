8d131863468320bab42de8aa1eac8e21b5626a93
thanhvp <phuocthanh20cgh@gmail.com>
2022-07-14 09:58:45 +0700

    Configuring React

--- Lab5_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/App.js
+++ Lab5_SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/App.js
@@ -5,21 +5,14 @@ import Menu from "./components/MenuComponent";
 import { DISHES } from "./shared/dishes";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES,
-    };
-  }
   render() {
     return (
       <div className="App">
-        <Navbar dark className="bg-[#d73232]">
+        <Navbar dark color="primary">
           <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusiont</NavbarBrand>
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
