497d4b135ebbba9fa846119ae5f7b05d90cf3ea4
thanhvp <phuocthanh20cgh@gmail.com>
2022-07-15 10:22:53 +0700

    Redux Thunk

--- Pe-SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/redux/configureStore.js
+++ Pe-SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/redux/configureStore.js
@@ -1,29 +1,27 @@
-import {createStore, combineReducers, applyMiddleware } from 'redux';
-import { Dishes } from './dishes';
-import { Comments } from './comments';
-import { Promotions } from './promotions';
-import { Leaders } from './leaders';
-import { createForms } from 'react-redux-form';
-import { InitialFeedback } from './forms';
-import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { Dishes } from "./dishes";
+import { Comments } from "./comments";
+import { Promotions } from "./promotions";
+import { Leaders } from "./leaders";
+import { createForms } from "react-redux-form";
+import { InitialFeedback } from "./forms";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
 
 export const ConfigureStore = () => {
-    const store = createStore(
-        combineReducers({
-            dishes: Dishes,
-            comments: Comments,
-            promotions: Promotions,
-            leaders: Leaders,
-            ...createForms({
-                feedback: InitialFeedback
-            })
-        }),
+  const store = createStore(
+    combineReducers({
+      dishes: Dishes,
+      comments: Comments,
+      promotions: Promotions,
+      leaders: Leaders,
+      ...createForms({
+        feedback: InitialFeedback,
+      }),
+    }),
 
-        applyMiddleware(thunk, logger)
-    );
-    
-    return store;
-}
+    applyMiddleware(thunk, logger)
+  );
 
-
+  return store;
+};
