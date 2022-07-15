2a41f3784404e4b498b1912ad3cce5293a221818
thanhvp <phuocthanh20cgh@gmail.com>
2022-07-15 10:27:45 +0700

    Redux Thunk

--- Pe-SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/redux/ActionCreators.js
+++ Pe-SE1632_VOPHUOCTHANH_SE151066/React/confusion/src/redux/ActionCreators.js
@@ -1,37 +1,34 @@
-import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes';
-
-
+import * as ActionTypes from "./ActionTypes";
+import { DISHES } from "../shared/dishes";
 
 export const addComment = (dishId, rating, author, comment) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        dishId: dishId,
-        rating: rating,
-        author: author,
-        comment: comment
-    }
+  type: ActionTypes.ADD_COMMENT,
+  payload: {
+    dishId: dishId,
+    rating: rating,
+    author: author,
+    comment: comment,
+  },
 });
 
 export const fetchDishes = () => (dispatch) => {
+  dispatch(dishesLoading(true));
 
-    dispatch(dishesLoading(true));
-
-    setTimeout(() => {
-        dispatch(addDishes(DISHES));
-    }, 2000);
-}
+  setTimeout(() => {
+    dispatch(addDishes(DISHES));
+  }, 2000);
+};
 
 export const dishesLoading = () => ({
-    type: ActionTypes.DISHES_LOADING
+  type: ActionTypes.DISHES_LOADING,
 });
 
 export const dishesFailed = (errmess) => ({
-    type: ActionTypes.DISHES_FAILED,
-    payload: errmess
+  type: ActionTypes.DISHES_FAILED,
+  payload: errmess,
 });
 
 export const addDishes = (dishes) => ({
-    type: ActionTypes.ADD_DISHES,
-    payload: dishes
+  type: ActionTypes.ADD_DISHES,
+  payload: dishes,
 });
